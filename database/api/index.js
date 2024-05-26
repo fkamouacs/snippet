2aedcca728936311edc85f9decdbf9a9d38d501b
fkamouacs <kamouaferid@gmail.com>
2024-05-26 13:31:44 -0400

    deploy

--- database/api/index.js
+++ database/api/index.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const db = require('./models/index');
+const db = require('../models/index');
 const dotenv = require('dotenv');
 
-const userRoutes = require('./routes/userRoutes');
-const quoteRoutes = require('./routes/quoteRoutes');
+const userRoutes = require('../routes/userRoutes');
+const quoteRoutes = require('../routes/quoteRoutes');
 
 dotenv.config();
 const PORT = process.env.PORT || 3001;
@@ -20,8 +20,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use('/users', userRoutes);
-app.use('/quotes', quoteRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/quotes', quoteRoutes);
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
