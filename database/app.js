b1d578844f21a00f253e8814a7a9a3d3ab15a9da
fkamouacs <kamouaferid@gmail.com>
2024-05-26 13:12:10 -0400

    cors

--- database/app.js
+++ database/app.js
@@ -11,7 +11,13 @@ const quoteRoutes = require('./routes/quoteRoutes');
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
