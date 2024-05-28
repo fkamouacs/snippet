984a38ba37f53a68072208bff4edd6a58f46550a
fkamouacs <kamouaferid@gmail.com>
2024-05-28 19:55:16 -0400

    deploy

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,29 +3,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
 const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
-exports.app = (0, express_1.default)();
-// import { router as userRoutes } from '../routes/userRoutes.ts';
-// import { router as quoteRoutes } from '../routes/quoteRoutes.ts';
-dotenv_1.default.config();
-const PORT = process.env.PORT || 3001;
+const app = (0, express_1.default)();
+const port = process.env.PORT || 8080;
 const corsOptions = {
     origin: '*',
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
 };
-exports.app.use((0, cors_1.default)(corsOptions));
-exports.app.use(express_1.default.json());
-// app.use('/api/users', userRoutes);
-// app.use('/api/quotes', quoteRoutes);
-// const adapter = new MongodbAdapter(
-//   db.collection('sessions'),
-//   db.collection('users')
-// );
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-exports.app.get('/', (req, res) => res.send('Express on Vercel'));
-exports.app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use((0, cors_1.default)(corsOptions));
+app.use(express_1.default.json());
+app.get('/', (_req, res) => {
+    return res.send('Express Typescript on Vercel');
+});
+app.get('/ping', (_req, res) => {
+    return res.send('pong 🏓');
+});
+app.listen(port, () => {
+    return console.log(`Server is listening on ${port}`);
+});
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,8 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import type { Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
-export const app = express();
-// import { db } from '../models/index.ts';
-import { Lucia } from 'lucia';
-import { MongodbAdapter } from '@lucia-auth/adapter-mongodb';
-// import { router as userRoutes } from '../routes/userRoutes.ts';
-// import { router as quoteRoutes } from '../routes/quoteRoutes.ts';
 
-dotenv.config();
-const PORT = process.env.PORT || 3001;
+const app = express();
+const port = process.env.PORT || 8080;
 
 const corsOptions = {
   origin: '*',
@@ -21,16 +13,14 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// app.use('/api/users', userRoutes);
-// app.use('/api/quotes', quoteRoutes);
+app.get('/', (_req: Request, res: Response) => {
+  return res.send('Express Typescript on Vercel');
+});
 
-// const adapter = new MongodbAdapter(
-//   db.collection('sessions'),
-//   db.collection('users')
-// );
+app.get('/ping', (_req: Request, res: Response) => {
+  return res.send('pong 🏓');
+});
 
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-app.get('/', (req: Request, res: Response) => res.send('Express on Vercel'));
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`);
+});
