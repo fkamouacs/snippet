c8e08ad0f60c067a96df6a20c652632a1539d6dc
fkamouacs <kamouaferid@gmail.com>
2024-05-28 19:53:39 -0400

    deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 import type { Request, Response } from 'express';
 import cors from 'cors';
 export const app = express();
-import { db } from '../models/index.ts';
+// import { db } from '../models/index.ts';
 import { Lucia } from 'lucia';
 import { MongodbAdapter } from '@lucia-auth/adapter-mongodb';
-import { router as userRoutes } from '../routes/userRoutes.ts';
-import { router as quoteRoutes } from '../routes/quoteRoutes.ts';
+// import { router as userRoutes } from '../routes/userRoutes.ts';
+// import { router as quoteRoutes } from '../routes/quoteRoutes.ts';
 
 dotenv.config();
 const PORT = process.env.PORT || 3001;
@@ -21,15 +21,15 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
-app.use('/api/quotes', quoteRoutes);
+// app.use('/api/users', userRoutes);
+// app.use('/api/quotes', quoteRoutes);
 
-const adapter = new MongodbAdapter(
-  db.collection('sessions'),
-  db.collection('users')
-);
+// const adapter = new MongodbAdapter(
+//   db.collection('sessions'),
+//   db.collection('users')
+// );
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', (req: Request, res: Response) => res.send('Express on Vercel'));
 
